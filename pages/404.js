2380af202ad6f9ee13f09a1316e7a1dcdec9e3d1
DM-Malkoff <dm.malkoff@gmail.com>
2022-08-17 20:03:01 +0400

    404 страница-исправление

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,11 @@
 import Link from 'next/link'
 import MainLayout from "../components/layouts/MainLayout";
-import MainSlider from "../components/mainSlider";
-import {getProductsData} from "../utils/products";
-import {quantityProductsMainSlider} from "../constants/config";
-import {getCategories} from "../utils/categories";
 
 const NotFoundPage = () => {
     return (
         <div className='not-found'>
             <MainLayout caption={"Упс... Страница не найдена"} lastNode={"404"}>
-                <span class="error-404">404</span>
+                <span className="error-404">404</span>
                 <p>Страница которую Вы ищете больше недоступна. Воспользуйтесь поиском или перейдите на <Link href='/'><a>главную страницу</a></Link> страницу.</p>
             </MainLayout>
         </div>
