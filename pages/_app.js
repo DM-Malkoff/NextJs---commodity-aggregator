d1b3bd911b96aec636fc36d40252a571bd48af30
DM-Malkoff <dm.malkoff@gmail.com>
2022-08-27 16:04:21 +0400

    sitemap.xml

--- pages/_app.js
+++ pages/_app.js
@@ -3,36 +3,24 @@ import '../styles/globals.scss'
 import React, {useState} from "react";
 import {Context} from "../context/context";
 import Router from "next/router";
-import Loader from "../components/loader";
-import Head from "next/head";
 import NProgress from "nprogress";
 NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }) {
   const [context, setContext] = useState('Test context')
-    const [loading, setLoading] = useState(false)
-
-    Router.events.on('routeChangeStart',(url)=>{
-        setLoading(true)
-        NProgress.start();
-    })
-    Router.events.on('routeChangeComplete',(url)=>{
-        setLoading(false)
-        NProgress.done();
-    })
+  Router.events.on('routeChangeStart',(url)=>{
+      NProgress.start();
+  })
+  Router.events.on('routeChangeComplete',(url)=>{
+    NProgress.done();
+  })
   return (
       <>
-          <Head>
-              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-                    integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
-                    crossOrigin="anonymous" referrerPolicy="no-referrer"/>
-          </Head>
-          {loading && <Loader />}
-      <div className='site__wrapper'>
-          <Context.Provider value={[context, setContext]}>
-            <Component {...pageProps} />
-          </Context.Provider>
-      </div>
+          <div className='site__wrapper'>
+              <Context.Provider value={[context, setContext]}>
+                <Component {...pageProps} />
+              </Context.Provider>
+          </div>
       </>
   )
 }
--- utils/products.js
+++ utils/products.js
@@ -2,8 +2,6 @@ import api from "../api";
 import {quantityProducts} from "../constants/config";
 
 export const getProductsData = async (queries,categoryId, numPage, qntProducts,orderby, order) => {
-    console.log("333",queries)
-
     const {slug: _, ...apiQueries} = queries
 
     let apiString = `products?per_page=${quantityProducts}&`
@@ -22,34 +20,8 @@ export const getProductsData = async (queries,categoryId, numPage, qntProducts,o
             apiString = apiString+myKey+'='+queries[key]
         }
     }
-    console.log('444', apiString)
+
     return await api.get(
         `${apiString}`
     );
-
-
-    // if (numPage){
-    //     return await api.get(
-    //         `products?category=${categoryId}&page=${numPage}&per_page=${qntProducts}`
-    //     );
-    // }
-    // else if (orderby){
-    //     if (order){
-    //         return await api.get(
-    //             `products?category=${categoryId}&page=1&per_page=${quantityProducts}&orderby=${orderby}&order=${order}`
-    //         );
-    //     } else {
-    //         return await api.get(
-    //             `products?category=${categoryId}&page=1&per_page=${quantityProducts}&orderby=${orderby}`
-    //         );
-    //     }
-    //
-    // }
-    // else{
-    //     return await api.get(
-    //         `products?category=${categoryId}&page=1&per_page=${quantityProducts}`
-    //     );
-    // }
-
-
 };
