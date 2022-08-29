b2f5176590eb8bf980125ec1c95137374241b7d9
DM-Malkoff <dm.malkoff@gmail.com>
2022-08-29 10:28:55 +0400

    адаптив фильтра+исправления

--- components/filter/filter.js
+++ components/filter/filter.js
@@ -15,7 +15,7 @@ export default function Filter() {
     }, [showFilterContext])
 
     const closeFilterButton = () => {
-        showFilterContext(false)
+        setShowFilterContext(false)
         setShowFilter(false)
     }
 
