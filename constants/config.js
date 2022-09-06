0fc5f67faa2b1d4224801c80d5b50473b079b030
DM-Malkoff <dm.malkoff@gmail.com>
2022-09-06 16:23:33 +0400

    шрифты

--- constants/config.js
+++ constants/config.js
@@ -14,19 +14,19 @@ export const sortSettings = [
     {sortName: '', sortType: 'clear', sortText: 'Не сортировать'}
 ]
 
-export const filterOptions = [
-    {categoryId: '27', attributeIds: [1, 2, 3, 4, 5, 6, 7,30,8], slug: 'diski'},
-    {categoryId: '5225', attributeIds: [1, 2, 3, 4, 5, 6, 7,8], slug: 'shtampovannye-diski'},
-    {categoryId: '5226', attributeIds: [1, 2, 3, 4, 5, 6, 7,8], slug: 'kovanye-diski'},
-    {categoryId: '5227', attributeIds: [1, 2, 3, 4, 5, 6, 7,8], slug: 'litye-diski'},
-
-    {categoryId: '5033', attributeIds: [1, 2, 3, 4, 5, 6,15,16,18], slug: 'gruzovye-shiny'},
-
-    {categoryId: '956', attributeIds: [1, 2], slug: 'shiny'},
-    {categoryId: '5229', attributeIds: [1, 2, 3, 4, 5, 6], slug: 'zimnie-shiny'},
-    {categoryId: '5230', attributeIds: [1, 2, 3, 4, 5, 6], slug: 'letnie-shiny'},
-
-    {categoryId: '4741', attributeIds: [1, 14,15, 16,34], slug: '/motoshiny'},
-    {categoryId: '5223', attributeIds: [1, 2, 3, 4, 5, 6], slug: '/zimnie-motoshiny'},
-    {categoryId: '5228', attributeIds: [1, 2, 3, 4, 5, 6], slug: '/letnie-motoshiny'},
-]
+// export const filterOptions = [
+//     {categoryId: '27', attributeIds: [1, 2, 3, 4, 5, 6, 7,30,8], slug: 'diski'},
+//     {categoryId: '5225', attributeIds: [1, 2, 3, 4, 5, 6, 7,8], slug: 'shtampovannye-diski'},
+//     {categoryId: '5226', attributeIds: [1, 2, 3, 4, 5, 6, 7,8], slug: 'kovanye-diski'},
+//     {categoryId: '5227', attributeIds: [1, 2, 3, 4, 5, 6, 7,8], slug: 'litye-diski'},
+//
+//     {categoryId: '5033', attributeIds: [1, 2, 3, 4, 5, 6,15,16,18], slug: 'gruzovye-shiny'},
+//
+//     {categoryId: '956', attributeIds: [1, 2], slug: 'shiny'},
+//     {categoryId: '5229', attributeIds: [1, 2, 3, 4, 5, 6], slug: 'zimnie-shiny'},
+//     {categoryId: '5230', attributeIds: [1, 2, 3, 4, 5, 6], slug: 'letnie-shiny'},
+//
+//     {categoryId: '4741', attributeIds: [1, 14,15, 16,34], slug: '/motoshiny'},
+//     {categoryId: '5223', attributeIds: [1, 2, 3, 4, 5, 6], slug: '/zimnie-motoshiny'},
+//     {categoryId: '5228', attributeIds: [1, 2, 3, 4, 5, 6], slug: '/letnie-motoshiny'},
+// ]
